26871393575309f292436212663c9d5625dce7de
sai hemanth <saihemanthjakkula@gmail.com>
2020-09-23 18:06:28 +0530

    DB details updated

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$connect = mysqli_connect("localhost", "root", "6325", "employees_dictionary");
+$connect = mysqli_connect("sql12.freesqldatabase.com", "sql12365554", "icz2ee5brJ", "sql12365554");
 
 if ($connect->connect_error) {
 	die("Connection Failed: " . $connect->connect_error);
